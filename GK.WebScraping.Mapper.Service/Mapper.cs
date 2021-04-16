8abcd9deccec27c90a9b67a63937d636b779d6d6
Gökhan Kutucularoglu <gkhanmk@hotmail.com>
2021-04-16 17:35:34 +0200

    Enteresaaaaan

--- GK.WebScraping.Mapper.Service/Mapper.cs
+++ GK.WebScraping.Mapper.Service/Mapper.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using GK.WebScraping.DB;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Linq;
+using GK.WebScraping.Model;
+using System.Collections.Generic;
 
 namespace GK.WebScraping.Mapper.Service
 {
@@ -17,9 +21,22 @@ namespace GK.WebScraping.Mapper.Service
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            //List<String> urls =
+            //    DatabaseManager.Instance.Pages
+            //    .Where(x => x.MapStatus == (Int32)StatusType.None)
+            //    .Select(x => x.Url)
+            //    .ToList();
+
+            //Start mapping from root...
+            List<String> rootUrls =
+                DatabaseManager.Instance.Stores
+                .Select(x => x.RootUrl).ToList();
+
+            _logger.LogInformation("Worker started at: {time}", DateTimeOffset.Now);
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
                 await Task.Delay(1000, stoppingToken);
             }
         }
---
+++ GK.WebScraping.Mapper.Service/Utilities/HtmlUtilities.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GK.WebScraping.Mapper.Service.Utilities
+{
+    public class HtmlUtilities
+    {
+
+    }
+}
